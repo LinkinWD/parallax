cb47c1286932500817666cc95b8e9a8880888748
yaram <yaramodesu@gmail.com>
2021-04-29 10:35:49 +0300

    parallax ja laxxx kokeilua

---
+++ scripts/animation.js
@@ -0,0 +1,10 @@
+anime({
+    targets: '.line',
+    keyframes: [
+        {translateY: 50},
+        {translateY: 0}
+    ],
+    delay: 500,
+    duration: 5000,
+    loop: true
+  });
--- scripts/parallax.js
+++ scripts/parallax.js
@@ -13,7 +13,7 @@ window.onload = function () {
         scrollY: {                // Driver name
             translateY: [           // CSS property
               [0,2000],  // Driver value map
-              [100,-500],   // Animation value map
+              [100,-400],   // Animation value map
             {
                 inertia: -5
             }
@@ -23,11 +23,10 @@ window.onload = function () {
         lax.addElements('.line', {
             scrollY: {                // Driver name
                 opacity: [           // CSS property
-                  [0,300],  // Driver value map
+                  [0,700],  // Driver value map
                   [1, 0]
               
                 ]
-               
             }})
             lax.addElements('.mountains', {
                 scrollY: {                // Driver name
@@ -44,7 +43,7 @@ window.onload = function () {
                     scrollY: {                // Driver name
                         translateY: [           // CSS property
                           [0,2000],  // Driver value map
-                          [0,-100],   // Animation value map
+                          [0,-200],   // Animation value map
                         {
                             inertia: -5
                         }
@@ -53,9 +52,9 @@ window.onload = function () {
                     }}) 
                     lax.addElements('.secondary', {
                         scrollY: {                // Driver name
-                            translateY: [           // CSS property
-                              [0,1000],  // Driver value map
-                              [0,-300],   // Animation value map
+                            translateX: [           // CSS property
+                              [10,1000],  // Driver value map
+                              [-1000,0],   // Animation value map
                             {
                                 inertia: -5
                             }
